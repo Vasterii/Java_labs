ec210ce570cee75fbaa863697f4d9fae67e8ac49
Vasterii <sviatoslav.hom@mail.ru>
2021-12-10 00:30:26 +0300

    Added class Main

---
+++ src/bsu/rfe/java/group7/lab5/khomenko/varB/Main.java
@@ -0,0 +1,14 @@
+package bsu.rfe.java.group7.lab5.khomenko.varB;
+
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        MainFrame frame = new MainFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
